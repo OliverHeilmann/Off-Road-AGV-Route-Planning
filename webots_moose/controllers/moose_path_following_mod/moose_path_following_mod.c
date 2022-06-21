898327add5dc182876c3e0a3cdbbec0d588ef617
OliverHeilmann <oheilmann5@gmail.com>
2022-06-21 01:51:43 +0100

    added webots scripts into repo. Webots now uses python config files on bootup to setup tests automatically

---
+++ webots_moose/controllers/moose_path_following_mod/moose_path_following_mod.c
@@ -0,0 +1,300 @@
+/*
+ * Copyright 1996-2021 Cyberbotics Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <webots/compass.h>
+#include <webots/gps.h>
+#include <webots/keyboard.h>
+#include <webots/motor.h>
+#include <webots/robot.h>
+
+////////// For Moose path following //////////
+#define PATH "/Users/Oliver/Documents/CODING/Python_Prgms/WeBots_ElevationMap/maps/elevationmap_vehicle_config.txt" // path to vehicle configuration file
+#define TIME_STEP 16
+// #define TARGET_POINTS_SIZE 11
+#define DISTANCE_TOLERANCE 1.5
+#define MAX_SPEED 7.0
+#define TURN_COEFFICIENT 4.0
+
+enum XYZAComponents { X = 0, Y, Z, ALPHA };
+enum Sides { LEFT, RIGHT };
+
+typedef struct _Vector {
+  double u;
+  double v;
+} Vector;
+
+static WbDeviceTag motors[8];
+static WbDeviceTag gps;
+static WbDeviceTag compass;
+
+// static Vector targets[TARGET_POINTS_SIZE] = {
+//   {-49.0,-49.0},{-10.0,-10.0},{-10.0,-8.0},{-12.0,-6.0},{-12.0,-5.0},{-12.0,-3.0},{-12.0,0.0},{-13.0,1.0},{-13.0,5.0},{30.0,48.0},{48.0,48.0}
+// };
+
+static int current_target_index = 0;
+static bool autopilot = true;
+static bool old_autopilot = true;
+static int old_key = -1;
+
+static double modulus_double(double a, double m) {
+  const int div = (int)(a / m);
+  double r = a - div * m;
+  if (r < 0.0)
+    r += m;
+  return r;
+}
+
+// set left and right motor speed [rad/s]
+static void robot_set_speed(double left, double right) {
+  int i;
+  for (i = 0; i < 4; i++) {
+    wb_motor_set_velocity(motors[i + 0], left);
+    wb_motor_set_velocity(motors[i + 4], right);
+  }
+}
+
+static void check_keyboard() {
+  double speeds[2] = {0.0, 0.0};
+
+  int key = wb_keyboard_get_key();
+  if (key >= 0) {
+    switch (key) {
+      case WB_KEYBOARD_UP:
+        speeds[LEFT] = MAX_SPEED;
+        speeds[RIGHT] = MAX_SPEED;
+        autopilot = false;
+        break;
+      case WB_KEYBOARD_DOWN:
+        speeds[LEFT] = -MAX_SPEED;
+        speeds[RIGHT] = -MAX_SPEED;
+        autopilot = false;
+        break;
+      case WB_KEYBOARD_RIGHT:
+        speeds[LEFT] = MAX_SPEED;
+        speeds[RIGHT] = -MAX_SPEED;
+        autopilot = false;
+        break;
+      case WB_KEYBOARD_LEFT:
+        speeds[LEFT] = -MAX_SPEED;
+        speeds[RIGHT] = MAX_SPEED;
+        autopilot = false;
+        break;
+      case 'P':
+        if (key != old_key) {  // perform this action just once
+          const double *position_3d = wb_gps_get_values(gps);
+          printf("position: {%f, %f}\n", position_3d[X], position_3d[Y]);
+        }
+        break;
+      case 'A':
+        if (key != old_key)  // perform this action just once
+          autopilot = !autopilot;
+        break;
+    }
+  }
+  if (autopilot != old_autopilot) {
+    old_autopilot = autopilot;
+    if (autopilot)
+      printf("auto control\n");
+    else
+      printf("manual control\n");
+  }
+
+  robot_set_speed(speeds[LEFT], speeds[RIGHT]);
+  old_key = key;
+}
+
+// ||v||
+static double norm(const Vector *v) {
+  return sqrt(v->u * v->u + v->v * v->v);
+}
+
+// v = v/||v||
+static void normalize(Vector *v) {
+  double n = norm(v);
+  v->u /= n;
+  v->v /= n;
+}
+
+// v = v1-v2
+static void minus(Vector *v, const Vector *const v1, const Vector *const v2) {
+  v->u = v1->u - v2->u;
+  v->v = v1->v - v2->v;
+}
+
+// autopilot
+// pass trough the predefined target positions
+static void run_autopilot( int *target_points_size, Vector *new_targets ) {
+  // prepare the speed array
+  double speeds[2] = {0.0, 0.0};
+
+  // read gps position and compass values
+  const double *position_3d = wb_gps_get_values(gps);
+  const double *north_3d = wb_compass_get_values(compass);
+
+  // compute the 2D position of the robot and its orientation
+  const Vector position = {position_3d[X], position_3d[Y]};
+
+  // compute the direction and the distance to the target
+  Vector direction;
+  minus(&direction, &(new_targets[current_target_index]), &position);
+  const double distance = norm(&direction);
+  normalize(&direction);
+
+  // compute the error angle
+  const double robot_angle = atan2(north_3d[0], north_3d[1]);
+  const double target_angle = atan2(direction.v, direction.u);
+  double beta = modulus_double(target_angle - robot_angle, 2.0 * M_PI) - M_PI;
+
+  // move singularity
+  if (beta > 0)
+    beta = M_PI - beta;
+  else
+    beta = -beta - M_PI;
+
+  // a target position has been reached
+  if (distance < DISTANCE_TOLERANCE) {
+    char index_char[3] = "th";
+    if (current_target_index == 0)
+      sprintf(index_char, "st");
+    else if (current_target_index == 1)
+      sprintf(index_char, "nd");
+    else if (current_target_index == 2)
+      sprintf(index_char, "rd");
+    printf("%d%s target reached\n", current_target_index + 1, index_char);
+    current_target_index++;
+    current_target_index %= *target_points_size;
+  }
+  // move the robot to the next target
+  else {
+    speeds[LEFT] = MAX_SPEED - M_PI + TURN_COEFFICIENT * beta;
+    speeds[RIGHT] = MAX_SPEED - M_PI - TURN_COEFFICIENT * beta;
+  }
+
+  // set the motor speeds
+  robot_set_speed(speeds[LEFT], speeds[RIGHT]);
+}
+
+// Open the vehicle configuration file and extract the translation and rotation values
+static Vector * vehicle_config ( int *target_point_size, Vector test_targets[] ) {
+  // create vector to append waypoints to
+  char * line = NULL;
+  size_t len = 0;
+  ssize_t read;
+  FILE * fp = fopen(PATH, "r");
+  if (fp == NULL)
+      exit(EXIT_FAILURE);
+
+  // loop through all the lines in text file
+  int row = 0, col = 0;
+  while ( (read = getline(&line, &len, fp)) != -1 ){
+    // loop through all the words in line
+    char *ptr = strtok(line, " ");
+    
+    col = 0;
+    while ( ptr != NULL ){
+      // row for getting the length of waypoints
+      if ( row == 4  && col == 1){
+        *target_point_size =  atof(ptr);
+        memset( test_targets, 0, *target_point_size*sizeof(int) );
+      }
+      // row for getting the waypoints
+      if ( row == 5 && col == 1){
+        double u, v;
+        int count = 0, step = 0;
+        char * token = strtok(ptr, "{},");
+        while( token != NULL ) {
+          // if even, set value to u
+          if (count % 2 == 0){
+            u = atof(token);
+          }
+          // if odd then set value to v, then push to targets
+          else {
+            v = atof(token);
+            Vector val = {u, v};
+            test_targets[step] = val;
+            step++;
+          }
+          token = strtok(NULL, "{},");
+          count++;
+        }
+      }
+      ptr = strtok(NULL, " ");
+      col++;
+    }
+    row++;
+  }
+  fclose(fp);
+  return test_targets;
+}
+
+int main(int argc, char *argv[]) {
+  // initialize webots communication
+  wb_robot_init();
+
+  // setup webots vehicle params for terrain tests
+  int target_points_size = 10;
+  Vector targets[target_points_size];
+  Vector *new_targets = vehicle_config( &target_points_size, targets );
+
+  // print user instructions
+  printf("You can drive this robot:\n");
+  printf("Select the 3D window and use cursor keys:\n");
+  printf("Press 'A' to return to the autopilot mode\n");
+  printf("Press 'P' to get the robot position\n");
+  printf("\n");
+
+  wb_robot_step(1000);
+
+  const char *names[8] = {"left motor 1",  "left motor 2",  "left motor 3",  "left motor 4",
+                          "right motor 1", "right motor 2", "right motor 3", "right motor 4"};
+
+  // get motor tags
+  int i;
+  for (i = 0; i < 8; i++) {
+    motors[i] = wb_robot_get_device(names[i]);
+    wb_motor_set_position(motors[i], INFINITY);
+  }
+
+  // get gps tag and enable
+  gps = wb_robot_get_device("gps");
+  wb_gps_enable(gps, TIME_STEP);
+
+  // get compass tag and enable
+  compass = wb_robot_get_device("compass");
+  wb_compass_enable(compass, TIME_STEP);
+
+  // enable keyboard
+  wb_keyboard_enable(TIME_STEP);
+
+  // start forward motion
+  robot_set_speed(MAX_SPEED, MAX_SPEED);
+
+  // main loop
+  while (wb_robot_step(TIME_STEP) != -1) {
+    check_keyboard();
+    if (autopilot)
+      run_autopilot( &target_points_size, new_targets );
+  }
+
+  wb_robot_cleanup();
+
+  return 0;
+}
---
+++ webots_moose/controllers/supervisor_draw_trail_mod/build/release/supervisor_draw_trail_mod.d
@@ -0,0 +1,6 @@
+build/release/supervisor_draw_trail_mod.o: supervisor_draw_trail_mod.c \
+  /Applications/Webots.app/include/controller/c/webots/robot.h \
+  /Applications/Webots.app/include/controller/c/webots/types.h \
+  /Applications/Webots.app/include/controller/c/webots/nodes.h \
+  /Applications/Webots.app/include/controller/c/webots/supervisor.h \
+  /Applications/Webots.app/include/controller/c/webots/contact_point.h
